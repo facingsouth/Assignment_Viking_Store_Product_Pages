7301ca84471492c4befcd3f2c75b7ee4fb260b70
Alice Fung <alice.o.fung@gmail.com>
2015-07-29 15:02:49 -0400

    radio buttons

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
         @user = User.find(params[:user_id])
         @orders = @user.orders.limit(100)
       else
-        flash[:error] = "No user with this ID found." 
+        flash[:error] = "No user with this ID found."
         redirect_to orders_path
       end
     else
@@ -23,10 +23,11 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(whitelisted_order_params)
     @user = User.find(params[:order][:user_id])
-    @cart_id=@user.orders.where("status=?", "Unplaced")
-    if @cart_id
+    @cart_id=@user.orders.where("checkout_date IS NULL")
+    if @cart_id.first
+      @order = @cart_id.first
       flash[:error]="Cart already exists, you can only have one cart"
-      redirect_to edit_order_path(@cart_id.first.id)
+      redirect_to edit_order_path(@order)
     else
         if @order.save
           flash[:success] = "New order created."
@@ -44,41 +45,39 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    @address = Address.find(params[:id])
-
-    @user = User.find(@address.user_id)
+    @order = Order.find(params[:id])
+    @user = User.find(@order.user_id)
   end
 
-  # def update
-  #   @address = Address.find(params[:id])
-  #   @address.city_id = check_city(params[:address][:city])
-  #   if @address.update(white_listed_address_params)
-  #     flash[:success] = "Address successfully modified."
-  #     redirect_to order_path
-  #   else
-  #     flash.now[:error] = @address.errors.full_messages.first
-  #     render :edit
-  #   end
-  # end
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(whitelisted_order_params)
+      flash[:success] = "order successfully modified."
+      redirect_to order_path
+    else
+      flash.now[:error] = @order.errors.full_messages.first
+      render :edit
+    end
+  end
 
-  # def destroy
-  #   session[:return_to] ||= request.referer
-  #   @address = Address.find(params[:id])
-  #   if @address.destroy
-  #     flash[:success] = "address successfully deleted."
-  #     redirect_to order_path
-  #   else
-  #     flash[:error] = @address.errors.full_messages.first
-  #     redirect_to session.delete(:return_to)
-  #   end
-  # end
+  def destroy
+    session[:return_to] ||= request.referer
+    @order = Order.find(params[:id])
+    if @order.destroy
+      flash[:success] = "Order successfully deleted."
+      redirect_to order_path
+    else
+      flash[:error] = @order.errors.full_messages.first
+      redirect_to session.delete(:return_to)
+    end
+  end
 
   private
 
   def whitelisted_order_params
-    params.require(:order).permit(:checkout_date, 
-                                    :user_id, 
-                                    :shipping_id, 
+    params.require(:order).permit(:checkout_date,
+                                    :user_id,
+                                    :shipping_id,
                                     :billing_id )
   end
 end
