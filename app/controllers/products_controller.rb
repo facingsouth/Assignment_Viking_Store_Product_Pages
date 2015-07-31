da1fcc7ee13c673d5ca1c843da4f888344c59c86
Alice Fung <alice.o.fung@gmail.com>
2015-07-31 14:36:08 -0400

    working filter w/ form tag

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,19 @@
 class ProductsController < ApplicationController
+
   def index
-    @products = Product.all
+    if whitelisted_params
+      @products = Product.where("category_id =?", whitelisted_params[:category_id].to_i)
+    else
+      @products = Product.all.limit(12)
+    end
+
+    render :index, locals: {:category_id => }
+  end
+
+
+  private
+
+  def whitelisted_params
+    params.require(:product).permit(:category_id)
   end
 end
